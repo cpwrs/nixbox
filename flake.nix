cf0661ba9adc590e25371400fa544193610fb2ab
Carson Powers <me@carsonp.net>
2024-12-17 12:42:56 -0600

    feat(evremap): add systemd service

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
 		nixosConfigurations = {
 			box = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
-				# Pass all inputs to the modules.
 				specialArgs.inputs = inputs;
 				modules = [
 					./conf.nix
