169000d83485ec633f08bd7de83af37a7e8efbcf
Carson Powers <me@carsonp.net>
2024-12-31 14:52:14 -0600

    feat(nix): add lsp

--- flake.nix
+++ flake.nix
@@ -1,24 +1,34 @@
 {
-	description = "NixOS 24.05 config for my desktop";
+  description = "NixOS 24.05 config for my desktop";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     neovim.url = "github:crpowers/envy";
     wezterm = {
       url = "github:wez/wezterm/main?dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	};
+  };
 
-	outputs = { self, nixpkgs, ... } @ inputs: {
-		nixosConfigurations = {
-			box = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				specialArgs.inputs = inputs;
-				modules = [
-					./conf.nix
-				];
-			};
-		};	
-	};
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let system = "x86_64-linux"; 
+    in {
+      nixosConfigurations = {
+        box = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs.inputs = inputs;
+          modules = [
+            ./conf.nix
+          ];
+        };
+      };	
+
+      devShells.${system}.default =
+        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+          packages = [
+            pkgs.nil # nix ls
+          ];
+        };
+    };
 }
