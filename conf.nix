9a2d7b02ae04fc50b5f5c2c9907b66a5a372c0ba
Carson Powers <me@carsonp.net>
2025-03-25 16:09:58 -0500

    feat: gc and store optimization

--- conf.nix
+++ conf.nix
@@ -13,6 +13,15 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "4:00" ];
+    };
   };
 
   # Use the systemd-boot EFI boot loader.
@@ -24,20 +33,6 @@
       };
       efi.canTouchEfiVariables = true;
     }; 
-  };
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
   };
 
   # Set timezone.
