f4a44c87a04aa2d1471f53682bf0dfcce3df3001
Carson Powers <me@carsonp.net>
2025-07-07 18:32:18 -0500

    feat(toaster): wallpaper

--- hosts/toaster/default.nix
+++ hosts/toaster/default.nix
@@ -37,6 +37,7 @@
       '';
     };
     variables = ["LIBVA_DRIVER_NAME,nvidia" "XDG_SESSION_TYPE,wayland" "GBM_BACKEND,nvidia-drm" "__GLX_VENDOR_LIBRARY_NAME,nvidia"];
+    wallpaper = ./wallpaper.jpg;
   };
 
   # Nvidia on Wayland...
