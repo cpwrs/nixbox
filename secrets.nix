8dd02a086300584c7c423066206eaf35e2e3c7fb
Carson Powers <me@carsonp.net>
2026-04-28 15:21:29 -0500

    feat: add toaster keys

---
+++ secrets.nix
@@ -0,0 +1,9 @@
+let
+  host = {
+    toaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBm+MfqJdf0kjwRzT3QPr4srZmYWl5qBbSIgPNLkkXM root@toaster";
+  };
+  carson = {
+    toaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9+4cvvVu5SOVi1/rxU6xhUcBAhW9frDaE0TI5MXrIX carson@toaster";
+  };
+in {
+}
