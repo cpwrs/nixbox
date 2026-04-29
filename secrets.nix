400c47b596af5508702afcdbb2f89f38adf7da2a
Carson Powers <me@carsonp.net>
2026-04-29 11:44:40 -0500

    feat: add surface keys

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,11 @@
 let
   host = {
     toaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBm+MfqJdf0kjwRzT3QPr4srZmYWl5qBbSIgPNLkkXM root@toaster";
+    surface = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5al1hlsLRpQxkMaGen3IMFKHSdmW1EhiIwEU/nP0iw root@surface";
   };
   carson = {
     toaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9+4cvvVu5SOVi1/rxU6xhUcBAhW9frDaE0TI5MXrIX carson@toaster";
+    surface = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFzh/HEQgeasLpvfHLPSqDNpxjFwMdTIRjZoLkfKDm8x carson@surface";
   };
 in {
 }
