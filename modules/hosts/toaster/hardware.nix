3084eaeb0212366e49a01ca177c6c2c48d5faba0
Carson Powers <me@carsonp.net>
2026-05-07 17:25:32 -0500

    fix: toaster is systemd-boot

--- modules/hosts/toaster/hardware.nix
+++ modules/hosts/toaster/hardware.nix
@@ -1,18 +1,13 @@
 {...}: {
   flake.modules.nixos.toaster = {
-    fileSystems."/" = {
-      device = "/dev/disk/by-uuid/33e8b576-10b2-4a1c-8763-da93f790d09d";
-      fsType = "ext4";
-    };
-
-    swapDevices = [
-      {device = "/dev/disk/by-uuid/a951d816-878d-4e91-96c7-475f40eb6fec";}
-    ];
-
-    boot.loader.grub = {
-      enable = true;
-      device = "/dev/nvme0n1";
-      useOSProber = true;
+    boot = {
+      loader = {
+        systemd-boot = {
+          enable = true;
+          configurationLimit = 5;
+        };
+        efi.canTouchEfiVariables = true;
+      };
     };
   };
 }
